9b784406a56f50eaa5f8437c56204b60f2876ca2
martinbaste <martinbaste@gmail.com>
2018-12-26 12:40:47 +0100

    Updates

---
+++ precache-manifest.23bd76cd74ace9cf5b330eceff945746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "92310e24b70046dddaa6",
+    "url": "/bingo/static/js/runtime~main.92310e24.js"
+  },
+  {
+    "revision": "972a5e983a1bbd6254f7",
+    "url": "/bingo/static/js/main.972a5e98.chunk.js"
+  },
+  {
+    "revision": "4fd2de64d7a6bfb3e59f",
+    "url": "/bingo/static/js/1.4fd2de64.chunk.js"
+  },
+  {
+    "revision": "972a5e983a1bbd6254f7",
+    "url": "/bingo/static/css/main.5ea09f23.chunk.css"
+  },
+  {
+    "revision": "4fd2de64d7a6bfb3e59f",
+    "url": "/bingo/static/css/1.07f8c2f5.chunk.css"
+  },
+  {
+    "revision": "dc9c91667a8b38539653f75005870a67",
+    "url": "/bingo/index.html"
+  }
+];
